600c06cd9460a2b4007b966549b30b480a30f86d
Vladis Markin <vladismarkin@gmail.com>
2020-11-05 23:09:40 +0200

    Importing the libaries

---
+++ malarian-cell-cnn.py
@@ -0,0 +1,9 @@
+"""
+CNN to classify healthy vs sick cells
+Dataset obtained from ftp://lhcftp.nlm.nih.gov/Open-Access-Datasets/Malaria/cell_images.zip
+"""
+
+import numpy as np
+np.random.seed(1000)
+import cv2
+import os
