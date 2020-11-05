8ec47ae152e18321ca0aa14056e83396d24745b4
Vladis Markin <vladismarkin@gmail.com>
2020-11-06 01:01:40 +0200

    2 ways to Creating the model

--- malarian-cell-cnn.py
+++ malarian-cell-cnn.py
@@ -4,6 +4,82 @@ Dataset obtained from ftp://lhcftp.nlm.nih.gov/Open-Access-Datasets/Malaria/cell
 """
 
 import numpy as np
+#Set the `numpy` pseudo-random generator at a fixed value
+#This helps with repeatable results everytime you run the code. 
+
 np.random.seed(1000)
-import cv2
+
+import matplotlib.pyplot as plt
 import os
+import cv2
+import keras
+from PIL import Image
+
+os.environ['KERAS_BACKEND'] = 'tensorflow' # Added to set the backend as Tensorflow
+
+
+#Iterate through all images in Parasitized folder, resize to 64 x 64
+#Then save as numpy array with name 'dataset'
+#Set the label to this as 0
+
+image_directory = 'C:/Users/Vladis/Desktop/cell_images/'
+SIZE = 64
+
+INPUT_SHAPE = (SIZE, SIZE, 3)
+inp = keras.layers.Input(shape=INPUT_SHAPE)
+
+conv1 = keras.layers.Conv2D(32, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(inp)
+pool1 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+norm1 = keras.layers.BatchNormalization(axis = -1)(pool1)
+drop1 = keras.layers.Dropout(rate=0.2)(norm1)
+conv2 = keras.layers.Conv2D(32, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(drop1)
+pool2 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+norm2 = keras.layers.BatchNormalization(axis = -1)(pool2)
+drop2 = keras.layers.Dropout(rate=0.2)(norm2)
+
+flat = keras.layers.Flatten()(drop2)  #Flatten the matrix to get it ready for dense.
+
+hidden1 = keras.layers.Dense(512, activation='relu')(flat)
+norm3 = keras.layers.BatchNormalization(axis = -1)(hidden1)
+drop3 = keras.layers.Dropout(rate=0.2)(norm3)
+hidden2 = keras.layers.Dense(256, activation='relu')(drop3)
+norm4 = keras.layers.BatchNormalization(axis = -1)(hidden2)
+drop4 = keras.layers.Dropout(rate=0.2)(norm4)
+
+out = keras.layers.Dense(2, activation='sigmoid')(drop4)   #units=1 gives error
+
+model = keras.Model(inputs=inp, outputs=out)
+model.compile(optimizer='adam',
+                loss='categorical_crossentropy',   #Check between binary_crossentropy and categorical_crossentropy
+                metrics=['accuracy'])
+print(model.summary())
+
+#################################################
+"""
+from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
+from keras.models import Sequential
+
+model = None
+model = Sequential()
+model.add(Convolution2D(32, (3, 3), input_shape = (SIZE, SIZE, 3), activation = 'relu', data_format='channels_last'))
+model.add(MaxPooling2D(pool_size = (2, 2), data_format="channels_last"))
+model.add(BatchNormalization(axis = -1))
+model.add(Dropout(0.2))
+model.add(Convolution2D(32, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2), data_format="channels_last"))
+model.add(BatchNormalization(axis = -1))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(activation = 'relu', units=512))
+model.add(BatchNormalization(axis = -1))
+model.add(Dropout(0.2))
+model.add(Dense(activation = 'relu', units=256))
+model.add(BatchNormalization(axis = -1))
+model.add(Dropout(0.2))
+model.add(Dense(activation = 'sigmoid', units=2))
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+print(model.summary())
+"""
+#################################################
