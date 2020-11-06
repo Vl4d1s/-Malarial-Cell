2abb0af37baa655a1470fab85fa1afa9655e8dc2
Vladis Markin <vladismarkin@gmail.com>
2020-11-06 12:03:56 +0200

    Runing the model

--- malarian-cell-cnn.py
+++ malarian-cell-cnn.py
@@ -14,6 +14,7 @@ import os
 import cv2
 import keras
 from PIL import Image
+from tqdm import tqdm 
 
 os.environ['KERAS_BACKEND'] = 'tensorflow' # Added to set the backend as Tensorflow
 
@@ -22,9 +23,30 @@ os.environ['KERAS_BACKEND'] = 'tensorflow' # Added to set the backend as Tensorf
 #Then save as numpy array with name 'dataset'
 #Set the label to this as 0
 
-image_directory = 'C:/Users/Vladis/Desktop/cell_images/'
+images_directory = 'C:/Users/Vladis/Desktop/cell_images/'
 SIZE = 64
+dataset = []
+label = []
 
+parasitized_images = os.listdir(images_directory + "Parasitized2")
+for i, image_name in tqdm(enumerate(parasitized_images), total=len(parasitized_images)):
+    if (image_name.split('.')[1] == "png"):
+        image = cv2.imread(images_directory + "Parasitized2/" + image_name)
+        image = Image.fromarray(image,'RGB')
+        image = image.resize((SIZE,SIZE))
+        dataset.append(np.array(image))
+        label.append(0)
+        
+uninfected_images = os.listdir(images_directory + "Uninfected2")
+for i, image_name in tqdm(enumerate(uninfected_images), total=len(uninfected_images)):
+    if (image_name.split('.')[1] == "png"):
+        image = cv2.imread(images_directory + "Uninfected2/" + image_name)
+        image = Image.fromarray(image,'RGB')
+        image = image.resize((SIZE,SIZE))
+        dataset.append(np.array(image))
+        label.append(1)
+        
+        
 INPUT_SHAPE = (SIZE, SIZE, 3)
 inp = keras.layers.Input(shape=INPUT_SHAPE)
 
@@ -56,6 +78,41 @@ model.compile(optimizer='adam',
                 metrics=['accuracy'])
 print(model.summary())
 
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
+X_train, X_test, y_train, y_test = train_test_split(dataset, to_categorical(np.array(label)), test_size = 0.20, random_state = 0)
+
+history = model.fit(np.array(X_train), y_train,batch_size=64, verbose=1,epochs=25,validation_split=0.1,shuffle=False)
+
+print("Test_Accuracy: {:.2f}%".format(model.evaluate(np.array(X_test), np.array(y_test))[1]*100))
+
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+t = f.suptitle('CNN Performance', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+max_epoch = len(history.history['accuracy'])+1
+epoch_list = list(range(1,max_epoch))
+ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(1, max_epoch, 5))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(1, max_epoch, 5))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
+
+
+#Save the model
+model.save('malaria_cnn.h5')
 #################################################
 """
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
